cbb95c0181f681c2fe225a3642ac131babe9612b
Jonor127-OP <n.beau@groupeonepoint.com>
2023-04-03 15:41:34 +0200

    add lm translation

--- dataset/nl/lm/prepare-wmt14en2de.sh
+++ dataset/nl/lm/prepare-wmt14en2de.sh
@@ -61,86 +61,86 @@ dev=dev/newstest2013
 
 mkdir -p $orig $tmp $prep
 
-#cd $orig
-#
-#for ((i=0;i<${#URLS[@]};++i)); do
-#    file=${FILES[i]}
-#    if [ -f $file ]; then
-#        echo "$file already exists, skipping download"
-#    else
-#        url=${URLS[i]}
-#        wget "$url"
-#        if [ -f $file ]; then
-#            echo "$url successfully downloaded."
-#        else
-#            echo "$url not successfully downloaded."
-#            exit -1
-#        fi
-#        if [ ${file: -4} == ".tgz" ]; then
-#            tar zxvf $file
-#        elif [ ${file: -4} == ".tar" ]; then
-#            tar xvf $file
-#        fi
-#    fi
-#done
-#cd ..
-#
-#echo "pre-processing train data..."
-#for l in $src $tgt; do
-#    rm $tmp/train.tags.$lang.tok.$l
-#    for f in "${CORPORA[@]}"; do
-#        cat $orig/$f.$l | \
-#            perl $NORM_PUNC $l | \
-#            perl $REM_NON_PRINT_CHAR | \
-#            perl $TOKENIZER -threads 8 -a -l $l >> $tmp/train.tags.$lang.tok.$l
-#    done
-#done
-#
-#echo "pre-processing test data..."
-#for l in $src $tgt; do
-#    if [ "$l" == "$src" ]; then
-#        t="src"
-#    else
-#        t="ref"
-#    fi
-#    grep '<seg id' $orig/test-full/newstest2014-deen-$t.$l.sgm | \
-#        sed -e 's/<seg id="[0-9]*">\s*//g' | \
-#        sed -e 's/\s*<\/seg>\s*//g' | \
-#        sed -e "s/\’/\'/g" | \
-#    perl $TOKENIZER -threads 8 -a -l $l > $tmp/test.$l
-#    echo ""
-#done
-#
-#pwd
-#
-#echo "get dev set"
-#for l in $src $tgt; do
-#    cp $orig/dev/newstest2013.$l $tmp/valid.$l
-#done
-#
-#for l in $src $tgt; do
-#    awk '{ print $0; }' $tmp/train.tags.$lang.tok.$l > $tmp/train.$l
-#done
-#
-#
-#TRAIN=$tmp/train.de-en
-#BPE_CODE=$prep/code
-#rm -f $TRAIN
-#for l in $src $tgt; do
-#    cat $tmp/train.$l >> $TRAIN
-#done
-#
-#echo "learn_bpe.py on ${TRAIN}..."
-#python $BPEROOT/learn_bpe.py -s $BPE_TOKENS < $TRAIN > $BPE_CODE
-#
-#for L in $src $tgt; do
-#    for f in train.$L valid.$L test.$L; do
-#        echo "apply_bpe.py to ${f}..."
-#        python $BPEROOT/apply_bpe.py -c $BPE_CODE < $tmp/$f > $tmp/bpe.$f
-#    done
-#done
-#
-#perl $CLEAN -ratio 1.5 $tmp/bpe.train $src $tgt $prep/train 1 250
+cd $orig
+
+for ((i=0;i<${#URLS[@]};++i)); do
+    file=${FILES[i]}
+    if [ -f $file ]; then
+        echo "$file already exists, skipping download"
+    else
+        url=${URLS[i]}
+        wget "$url"
+        if [ -f $file ]; then
+            echo "$url successfully downloaded."
+        else
+            echo "$url not successfully downloaded."
+            exit -1
+        fi
+        if [ ${file: -4} == ".tgz" ]; then
+            tar zxvf $file
+        elif [ ${file: -4} == ".tar" ]; then
+            tar xvf $file
+        fi
+    fi
+done
+cd ..
+
+echo "pre-processing train data..."
+for l in $src $tgt; do
+    rm $tmp/train.tags.$lang.tok.$l
+    for f in "${CORPORA[@]}"; do
+        cat $orig/$f.$l | \
+            perl $NORM_PUNC $l | \
+            perl $REM_NON_PRINT_CHAR | \
+            perl $TOKENIZER -threads 8 -a -l $l >> $tmp/train.tags.$lang.tok.$l
+    done
+done
+
+echo "pre-processing test data..."
+for l in $src $tgt; do
+    if [ "$l" == "$src" ]; then
+        t="src"
+    else
+        t="ref"
+    fi
+    grep '<seg id' $orig/test-full/newstest2014-deen-$t.$l.sgm | \
+        sed -e 's/<seg id="[0-9]*">\s*//g' | \
+        sed -e 's/\s*<\/seg>\s*//g' | \
+        sed -e "s/\’/\'/g" | \
+    perl $TOKENIZER -threads 8 -a -l $l > $tmp/test.$l
+    echo ""
+done
+
+pwd
+
+echo "get dev set"
+for l in $src $tgt; do
+    cp $orig/dev/newstest2013.$l $tmp/valid.$l
+done
+
+for l in $src $tgt; do
+    awk '{ print $0; }' $tmp/train.tags.$lang.tok.$l > $tmp/train.$l
+done
+
+
+TRAIN=$tmp/train.de-en
+BPE_CODE=$prep/code
+rm -f $TRAIN
+for l in $src $tgt; do
+    cat $tmp/train.$l >> $TRAIN
+done
+
+echo "learn_bpe.py on ${TRAIN}..."
+python $BPEROOT/learn_bpe.py -s $BPE_TOKENS < $TRAIN > $BPE_CODE
+
+for L in $src $tgt; do
+    for f in train.$L valid.$L test.$L; do
+        echo "apply_bpe.py to ${f}..."
+        python $BPEROOT/apply_bpe.py -c $BPE_CODE < $tmp/$f > $tmp/bpe.$f
+    done
+done
+
+perl $CLEAN -ratio 1.5 $tmp/bpe.train $src $tgt $prep/train 1 250
 
 for L in $src $tgt; do
     cp $tmp/bpe.valid.$L $prep/valid.$L
