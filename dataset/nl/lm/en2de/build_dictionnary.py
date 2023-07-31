b318593abbc32815fbcf78165b8c765f7c2c82da
Nathanael BEAU <n.beau@groupeonepoint.com>
2023-07-31 15:13:00 +0200

    good lm preprocessing for en2fr

---
+++ dataset/nl/lm/en2de/build_dictionnary.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+from collections import OrderedDict
+import fileinput
+import sys
+
+import numpy
+import json
+
+
+def main():
+    i = 5
+    worddict = OrderedDict()
+    worddict['<eos>'] = 1
+    worddict['<sos>'] = 2
+    worddict['<unk>'] = 3
+    worddict['<pad>'] = 0
+    worddict['<sep>'] = 4
+
+    for filename in sys.argv[1:]:
+        print('Processing', filename)
+        with open(filename, 'r', encoding='utf-8') as f:
+            for line in f:
+                words_in = line.strip().split(' ')
+                for w in words_in:
+                    if w not in worddict:
+                        worddict[w] = i
+                        i += 1
+
+        # The JSON RFC requires that JSON text be represented using either
+        # UTF-8, UTF-16, or UTF-32, with UTF-8 being recommended.
+        # We use UTF-8 regardless of the user's locale settings.
+        with open('%s.json'%filename, 'w', encoding='utf-8') as f:
+            json.dump(worddict, f, indent=2, ensure_ascii=False)
+
+        print('Done')
+
+
+if __name__ == '__main__':
+    main()
---
+++ dataset/nl/lm/en2de/tokenized_to_ids.py
@@ -0,0 +1,52 @@
+import argparse
+import json
+import gzip
+
+
+def transform_to_ids(vocab_file, tokenized_file, output_file):
+    # Load the vocabulary from the vocab_file
+    with open(vocab_file, 'r') as f:
+        vocab = json.load(f)
+
+    # Create a reverse vocabulary, mapping id -> token
+    reverse_vocab = {id: token for token, id in vocab.items()}
+
+    # Open the tokenized file and the output file
+    with open(tokenized_file, 'r') as tokenized_f, gzip.open(output_file, 'w') as output_f:
+        # Iterate through each line in the tokenized file
+        for line in tokenized_f:
+            # Tokenize the line
+            tokens = line.strip().split()
+
+            if '„' in tokens:
+                tokens = ['&quot;' if token == '„' else token for token in tokens]
+            if '“' in tokens:
+                tokens = ['&quot;' if token == '“' else token for token in tokens]
+            if '̱' in tokens:
+                tokens = [token for token in tokens if token != '̱']
+            if '–' in tokens:
+                tokens = ['-' if token == '–' else token for token in tokens]
+            if '´' in tokens:
+                tokens = [token for token in tokens if token != '´']
+            if '"' in tokens:
+                tokens = ['&quot;' if token == '"' else token for token in tokens]
+            if '@@\'' in tokens:
+                tokens = ['&quot;' if token == '@@\'' else token for token in tokens]
+
+            # Convert the tokens to ids using the vocabulary
+            ids = [vocab['<sos>']] + [vocab[token] if token in vocab else 3 for token in tokens] + [vocab['<eos>']]
+
+            # Write the ids to the output file
+            output_f.write((" ".join([str(id) for id in ids]) + "\n").encode())
+
+
+if __name__ == "__main__":
+    # Parse the command-line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("vocab_file", type=str, help="The vocabulary file (in JSON format)")
+    parser.add_argument("tokenized_file", type=str, help="The tokenized file")
+    parser.add_argument("output_file", type=str, help="The output file (will be gzip-compressed)")
+    args = parser.parse_args()
+
+    # Call the transform_to_ids function with the command-line arguments
+    transform_to_ids(args.vocab_file, args.tokenized_file, args.output_file)
