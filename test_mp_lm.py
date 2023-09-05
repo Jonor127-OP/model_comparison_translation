82d74015f724043d12f29c4533bca829951589e3
w.ameur <w.ameur@groupeonepoint.com>
2023-09-05 16:14:23 +0200

    modify testmplm

--- test_mp_lm.py
+++ test_mp_lm.py
@@ -111,28 +111,33 @@ def test(dataset_option):
     predicted = []
     pairs = []
 
-    for i, tgt_test in enumerate(test_loader):
-        tgt_dev_input, tgt_dev_output = get_input_output_lm(tgt_test, window=0)
+    # for i, tgt_test in enumerate(test_loader):
+    #     tgt_dev_input, tgt_dev_output = get_input_output_lm(tgt_test, window=0)
 
-        if i == 0:
-           tgt_dev = tgt_dev_input
-    # for src_dev, tgt_dev in test_loader:
-    #     src_mask = src_dev != 0
-    #     src_mask = src_mask[:, None, None, :]
-        sample = model.generate_greedy(tgt_dev_input, MAX_LEN, cuda=True)
+    #     if i == 0:
+    #        tgt_dev = tgt_dev_input
+
+        # sample = model.generate_greedy(tgt_dev_input, MAX_LEN, cuda=True)   
+        #  
+    for src_dev, tgt_dev in test_loader:
+        src_mask = src_dev != 0
+        src_mask = src_mask[:, None, None, :]
+
+
+        sample = model.generate_greedy(src_dev, src_mask, MAX_LEN)
 
         sample = accelerator.gather(sample)
         tgt_dev = accelerator.gather(tgt_dev)
         
          # Remove special characters using regex
-        source_str = re.sub(r'(@@ )|(@@ ?$)', '', ' '.join(' '.join(ids_to_tokens(tgt_dev_input.tolist()[i], vocabulary)) for i in range(tgt_dev_input.shape[0])))
-        target_str = re.sub(r'(@@ )|(@@ ?$)', '', ' '.join(' '.join(ids_to_tokens(tgt_dev_output.tolist()[i][1:], vocabulary)) for i in range(tgt_dev.shape[0])))
+        source_str = re.sub(r'(@@ )|(@@ ?$)', '', ' '.join(' '.join(ids_to_tokens(src_dev.tolist()[i], vocabulary)) for i in range(tgt_dev_input.shape[0])))
+        target_str = re.sub(r'(@@ )|(@@ ?$)', '', ' '.join(' '.join(ids_to_tokens(tgt_dev.tolist()[i][1:], vocabulary)) for i in range(tgt_dev.shape[0])))
         predicted_str = re.sub(r'(@@ )|(@@ ?$)', '', ' '.join(' '.join(ids_to_tokens(sample.tolist()[i][1:], vocabulary)) for i in range(sample.shape[0])))
 
         
         pairs.append([source_str, target_str, predicted_str])
 
-        target.append([ids_to_tokens(tgt_test.tolist()[0][1:], vocabulary)])
+        target.append([ids_to_tokens(tgt_dev.tolist()[0][1:], vocabulary)])
         predicted.append([ids_to_tokens(sample.tolist()[0][1:], vocabulary)])
 
     target_bleu = [BPE_to_eval(sentence, lm=True) for sentence in target]
@@ -153,6 +158,8 @@ def test(dataset_option):
         for i, pair in enumerate(pairs):
            row = [pair[0], pair[1], pair[2]]
            writer.writerow(row)
+
+    print('Comparison results exported to', output_file)
 
 
 if __name__ == '__main__':
